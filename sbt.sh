2012204cb231069c5a580de3e355f7bae19744ac
am_dev <amdirtylabdeveloper@gmail.com>
2017-03-31 13:15:21 -0400

    testing commit

--- sbt.sh
+++ sbt.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 java -Xmx3g -Xms1g -XX:+TieredCompilation -XX:ReservedCodeCacheSize=256m -XX:MaxPermSize=512m -XX:+UseNUMA -XX:+UseParallelGC -XX:+CMSClassUnloadingEnabled -jar sbt-launch.jar "$@"
+
+
--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -9,4 +9,13 @@ import doodle.backend.StandardInterpreter._
 // Example.image.draw
 object Example {
   val image = circle(10) on circle(20) on circle(30)
+  val manyShapes = (rectangle(500,50) fillColor Color.blue) above
+    ((circle(100) fillColor Color.red.spin(15.degrees)) beside
+    (circle(50) fillColor Color.red.spin(30.degrees)) beside
+    (circle(100) fillColor Color.red.spin(15.degrees))) above
+    (rectangle(500,50) fillColor Color.blue)
+  def main(args:Array[String])={
+    manyShapes.draw
+  }
 }
+
